37c90ab615f0b55bb4dd295fd4ed3a628ff917cc
Dmitry <hldimon@mail.ru>
2019-12-11 16:30:37 +0300

    Managing ventilation speed

---
+++ EventHandlers/Scenarios/VentilationSpeedManager.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using SmartHomeApi.Core.Interfaces;
+
+namespace Scenarios
+{
+    class VentilationSpeedManager
+    {
+        private readonly TimeSpan _measurementPeriod;
+        private readonly TimeSpan _halfMeasurementPeriod;
+        private int _minSpeed = 1;
+        private int _firstLevelDefaultSpeed = 4;
+        private int _secondLevelDefaultSpeed = 7;
+        private int _maxSpeed = 10;
+        private int _recommendedSpeed;
+
+        private readonly ConcurrentDictionary<string, List<StateChangedEvent>> _devicesTelemetry =
+            new ConcurrentDictionary<string, List<StateChangedEvent>>();
+
+        public VentilationSpeedManager(TimeSpan measurementPeriod)
+        {
+            _recommendedSpeed = _minSpeed;
+            _measurementPeriod = measurementPeriod;
+            _halfMeasurementPeriod = _measurementPeriod / 2;
+        }
+
+        public void AddEvent(StateChangedEvent args)
+        {
+            if (!_devicesTelemetry.ContainsKey(args.DeviceId))
+                _devicesTelemetry.AddOrUpdate(args.DeviceId, new List<StateChangedEvent>(),
+                    (s, list) => new List<StateChangedEvent>());
+
+            var events = _devicesTelemetry[args.DeviceId];
+
+            events.Add(args);
+
+            _devicesTelemetry[args.DeviceId] = GetFilteredData(events);
+        }
+
+        private List<StateChangedEvent> GetFilteredData(List<StateChangedEvent> events)
+        {
+            var now = DateTimeOffset.Now;
+            var startOfPeriod = now - _measurementPeriod;
+
+            if (events.Any(e => e.EventDate < startOfPeriod))
+                events = events.Where(e => e.EventDate >= startOfPeriod).ToList();
+
+            return events;
+        }
+
+        public int GetRecommendedSpeed()
+        {
+            var now = DateTimeOffset.Now;
+            var middleOfMeasurementPeriod = now - _halfMeasurementPeriod;
+
+            var recommendedSpeeds = new Dictionary<string, int>();
+
+            foreach (var pair in _devicesTelemetry)
+            {
+                var values = pair.Value.OrderBy(e => e.EventDate).ToList();
+
+                if (!values.Any())
+                {
+                    recommendedSpeeds.Add(pair.Key, _minSpeed);
+                    continue;
+                }
+
+                var beginValues = new List<int>();
+                var endValues = new List<int>();
+
+                if (values.Count == 1)
+                {
+                    var ev = values.First();
+
+                    if (string.IsNullOrWhiteSpace(ev.OldValue))
+                    {
+                        int speed = GetInitialRecommendedSpeed(ev.NewValue);
+
+                        recommendedSpeeds.Add(pair.Key, speed);
+                        continue;
+                    }
+
+                    var res1 = int.TryParse(ev.OldValue, NumberStyles.Any, CultureInfo.InvariantCulture,
+                        out var beginCo2ppm);
+                    var res2 = int.TryParse(ev.NewValue, NumberStyles.Any, CultureInfo.InvariantCulture,
+                        out var endCo2ppm);
+
+                    if (!res1 || !res2)
+                    {
+                        recommendedSpeeds.Add(pair.Key, _minSpeed);
+                        continue;
+                    }
+
+                    beginValues.Add(beginCo2ppm);
+                    endValues.Add(endCo2ppm);
+                }
+                else
+                {
+                    if (int.TryParse(values.First().OldValue, NumberStyles.Any, CultureInfo.InvariantCulture,
+                        out var co2ppm))
+                        beginValues.Add(co2ppm);
+
+                    var beginEvents = values.Where(e => e.EventDate < middleOfMeasurementPeriod).ToList();
+                    var endEvents = values.Where(e => e.EventDate >= middleOfMeasurementPeriod).ToList();
+
+                    if (!endEvents.Any())
+                    {
+                        //Take last event from beginning half of events
+                        var last = beginEvents.Last();
+                        endEvents.Add(last);
+                        beginEvents.Remove(last);
+                    }
+
+                    foreach (var @event in beginEvents)
+                    {
+                        if (int.TryParse(@event.NewValue, NumberStyles.Any, CultureInfo.InvariantCulture,
+                            out co2ppm))
+                            beginValues.Add(co2ppm);
+                    }
+
+                    foreach (var @event in endEvents)
+                    {
+                        if (int.TryParse(@event.NewValue, NumberStyles.Any, CultureInfo.InvariantCulture,
+                            out co2ppm))
+                            endValues.Add(co2ppm);
+                    }
+                }
+
+                if (!beginValues.Any())
+                {
+                    var average = (int)Math.Round(endValues.Average(), 0);
+
+                    int speed = GetInitialRecommendedSpeed(average);
+
+                    recommendedSpeeds.Add(pair.Key, speed);
+                    continue;
+                }
+
+                var beginAverage = (int)Math.Round(beginValues.Average(), 0);
+                var endAverage = (int)Math.Round(endValues.Average(), 0);
+
+                var changeDuringPeriod = endAverage - beginAverage;
+                var rec = GetRecommendedSpeed(changeDuringPeriod, endAverage);
+                recommendedSpeeds.Add(pair.Key, rec);
+            }
+
+            int recommendedSpeed = recommendedSpeeds.Max(p => p.Value);
+
+            _recommendedSpeed = recommendedSpeed;
+            return _recommendedSpeed;
+        }
+
+        private int GetInitialRecommendedSpeed(string co2ppmStr)
+        {
+            if (string.IsNullOrWhiteSpace(co2ppmStr) || !int.TryParse(co2ppmStr, NumberStyles.Any,
+                    CultureInfo.InvariantCulture, out var co2ppm))
+            {
+                return _minSpeed;
+            }
+
+            return GetInitialRecommendedSpeed(co2ppm);
+        }
+
+        private int GetInitialRecommendedSpeed(int co2ppm)
+        {
+            if (co2ppm <= 600)
+            {
+                return _minSpeed;
+            }
+
+            if (co2ppm > 600 && co2ppm <= 800)
+            {
+                return _firstLevelDefaultSpeed;
+            }
+
+            if (co2ppm > 800 && co2ppm <= 1000)
+            {
+                return _secondLevelDefaultSpeed;
+            }
+
+            return _maxSpeed;
+        }
+
+        private int GetRecommendedSpeed(int changeDuringPeriod, int endAverage)
+        {
+            var recommendedSpeed = _recommendedSpeed;
+
+            //Concentration is decreasing
+            if (changeDuringPeriod < 0)
+            {
+                var change = Math.Abs(changeDuringPeriod);
+
+                if (change <= 10)
+                {
+                    if (endAverage > 800) //Accelerate decreasing
+                        recommendedSpeed = _recommendedSpeed + 1;
+                }
+                else
+                {
+                    //Air is clear enough so can decrease speed
+                    if (endAverage < 800)
+                        recommendedSpeed = _recommendedSpeed - 1;
+                    //Otherwise leave speed as is
+                }
+            }
+            else if (changeDuringPeriod == 0)
+            {
+                if (endAverage > 800) //Initiate decreasing
+                    recommendedSpeed = _recommendedSpeed + 1;
+                //Otherwise leave speed as is
+            }
+            else //Concentration is increasing
+            {
+                if (changeDuringPeriod <= 10)
+                {
+                    if (endAverage > 800) //Try to decrease
+                        recommendedSpeed = _recommendedSpeed + 1;
+                }
+                else
+                {
+                    if (endAverage > 600) //Try to decrease
+                        recommendedSpeed = _recommendedSpeed + 1;
+                }
+            }
+
+            if (recommendedSpeed < _minSpeed)
+                recommendedSpeed = _minSpeed;
+
+            if (recommendedSpeed > _maxSpeed)
+                recommendedSpeed = _maxSpeed;
+
+            return recommendedSpeed;
+        }
+    }
+}
--- EventHandlers/Scenarios/VentilationSystem.cs
+++ EventHandlers/Scenarios/VentilationSystem.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Threading.Tasks;
 using SmartHomeApi.Core.Interfaces;
 
@@ -7,9 +9,73 @@ namespace Scenarios
     public class VentilationSystem : StateChangedSubscriberAbstract
     {
         private readonly int _failoverActionIntervalSeconds = 30;
+        private readonly VentilationSpeedManager _ventilationSpeedManager;
+        private Task _worker;
+        private TimeSpan _measurementPeriod = new TimeSpan(0, 0, 5, 0);
+        private bool _firstWorkerRun = true;
+        private int _firstWorkerRunDelaySeconds = 10;
+
+        private const string ToiletMega2560 = "Toilet_Mega2560";
+        private const string BedroomMega2560 = "Bedroom_Mega2560";
+        private const string KitchenMega2560 = "Kitchen_Mega2560";
+        private const string LivingRoomMega2560 = "LivingRoom_Mega2560";
+        private const string Breezart = "Breezart";
 
         public VentilationSystem(IApiManager manager, IItemHelpersFabric helpersFabric) : base(manager, helpersFabric)
         {
+            _ventilationSpeedManager = new VentilationSpeedManager(_measurementPeriod);
+
+            RunVentilationSpeedManagerWorker();
+        }
+
+        private void RunVentilationSpeedManagerWorker()
+        {
+            if (_worker == null || _worker.IsCompleted)
+            {
+                _worker = Task.Factory.StartNew(VentilationSpeedManagerWorkerWrapper).Unwrap().ContinueWith(
+                    t =>
+                    {
+                        Logger.Error(t.Exception);
+                    },
+                    TaskContinuationOptions.OnlyOnFaulted);
+            }
+        }
+
+        private async Task VentilationSpeedManagerWorkerWrapper()
+        {
+            while (true)
+            {
+                if (!_firstWorkerRun)
+                    await Task.Delay(_measurementPeriod).ConfigureAwait(false);
+                else
+                    await Task.Delay(_firstWorkerRunDelaySeconds * 1000).ConfigureAwait(false); //Wait a bit before first run
+
+                _firstWorkerRun = false;
+
+                try
+                {
+                    await VentilationSpeedManagerWorker().ConfigureAwait(false);
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+
+        private async Task VentilationSpeedManagerWorker()
+        {
+            var breezartManualManagement = Manager.GetState("Virtual_States", "BreezartManualManagement");
+
+            //If not parsed or manual management then return
+            if (breezartManualManagement == null ||
+                !bool.TryParse(breezartManualManagement.ToString(), out var value) || value)
+                return;
+
+            var recommendedSpeed = _ventilationSpeedManager.GetRecommendedSpeed();
+
+            await Manager.SetValue(Breezart, "SetSpeed", recommendedSpeed.ToString(CultureInfo.InvariantCulture))
+                         .ConfigureAwait(false);
         }
 
         protected override async Task ProcessNotification(StateChangedEvent args)
@@ -24,6 +90,15 @@ namespace Scenarios
             {
                 case "Virtual_States":
                     await ProcessVirtualStateEvents(args).ConfigureAwait(false);
+                    break;
+                case BedroomMega2560:
+                    await ProcessBedroomMega2560StateEvents(args).ConfigureAwait(false);
+                    break;
+                case KitchenMega2560:
+                    await ProcessKitchenMega2560StateEvents(args).ConfigureAwait(false);
+                    break;
+                case LivingRoomMega2560:
+                    await ProcessLivingRoomMega2560StateEvents(args).ConfigureAwait(false);
                     break;
             }
         }
@@ -84,11 +159,11 @@ namespace Scenarios
         {
             var commands = new List<Task<ISetValueResult>> { Manager.SetValue("Breezart", "UnitState", "Off") };
 
-            var currentToiletVentStatus = Manager.GetState("Toilet_Mega2560", "pin2");
+            var currentToiletVentStatus = Manager.GetState(ToiletMega2560, "pin2");
 
             //Turn off toilet ventilator
-            if (currentToiletVentStatus?.ToString() == "true")
-                commands.Add(Manager.SetValue("Toilet_Mega2560", "pin2", "pimp"));
+            if (currentToiletVentStatus?.ToString().ToLowerInvariant() == "true")
+                commands.Add(Manager.SetValue(ToiletMega2560, "pin2", "pimp"));
 
             return commands;
         }
@@ -97,11 +172,11 @@ namespace Scenarios
         {
             var commands = new List<Task<ISetValueResult>> { Manager.SetValue("Breezart", "UnitState", "On") };
 
-            var currentToiletVentStatus = Manager.GetState("Toilet_Mega2560", "pin2");
+            var currentToiletVentStatus = Manager.GetState(ToiletMega2560, "pin2");
 
             //Turn on toilet ventilator
-            if (currentToiletVentStatus?.ToString() == "false")
-                commands.Add(Manager.SetValue("Toilet_Mega2560", "pin2", "pimp"));
+            if (currentToiletVentStatus?.ToString().ToLowerInvariant() == "false")
+                commands.Add(Manager.SetValue(ToiletMega2560, "pin2", "pimp"));
 
             return commands;
         }
@@ -110,11 +185,11 @@ namespace Scenarios
         {
             var commands = new List<Task<ISetValueResult>> { Manager.SetValue("Breezart", "UnitState", "On") };
 
-            var currentToiletVentStatus = Manager.GetState("Toilet_Mega2560", "pin2");
+            var currentToiletVentStatus = Manager.GetState(ToiletMega2560, "pin2");
 
             //Turn off toilet ventilator
-            if (currentToiletVentStatus?.ToString() == "true")
-                commands.Add(Manager.SetValue("Toilet_Mega2560", "pin2", "pimp"));
+            if (currentToiletVentStatus?.ToString().ToLowerInvariant() == "true")
+                commands.Add(Manager.SetValue(ToiletMega2560, "pin2", "pimp"));
 
             return commands;
         }
@@ -124,6 +199,48 @@ namespace Scenarios
             var commands = new List<Task<ISetValueResult>> { Manager.SetValue("Breezart", "UnitState", "On") };
 
             return commands;
+        }
+
+        private async Task ProcessBedroomMega2560StateEvents(StateChangedEvent args)
+        {
+            switch (args.Parameter)
+            {
+                case "CO2ppm":
+                    await ProcessCO2Parameter(args).ConfigureAwait(false);
+                    break;
+            }
+        }
+
+        private async Task ProcessKitchenMega2560StateEvents(StateChangedEvent args)
+        {
+            switch (args.Parameter)
+            {
+                case "CO2ppm":
+                    await ProcessCO2Parameter(args).ConfigureAwait(false);
+                    break;
+            }
+        }
+
+        private async Task ProcessLivingRoomMega2560StateEvents(StateChangedEvent args)
+        {
+            switch (args.Parameter)
+            {
+                case "CO2ppm":
+                    await ProcessCO2Parameter(args).ConfigureAwait(false);
+                    break;
+            }
+        }
+
+        private async Task ProcessCO2Parameter(StateChangedEvent args)
+        {
+            try
+            {
+                _ventilationSpeedManager.AddEvent(args);
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e);
+            }
         }
     }
 }
