fbb04c97b11bea20c5f44e8a45c3910185006d05
Dmitry <hldimon@mail.ru>
2019-12-20 17:52:59 +0300

    Async Lazy

---
+++ Common.Utils/AsyncLazy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Common.Utils
+{
+    public class AsyncLazy : Lazy<Task>
+    {
+        public AsyncLazy(Func<Task> taskFactory) : base(() => Task.Run(taskFactory))
+        {
+        }
+    }
+
+    public class AsyncLazy<T> : Lazy<Task<T>>
+    {
+        public AsyncLazy(Func<T> valueFactory) : base(() => Task.Factory.StartNew(valueFactory))
+        {
+        }
+
+        public AsyncLazy(Func<Task<T>> taskFactory) : base(() => Task.Run(taskFactory))
+        {
+        }
+    }
+}
