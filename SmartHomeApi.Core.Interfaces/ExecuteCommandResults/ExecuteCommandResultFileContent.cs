ac084bfddc162c3e6c695e32bc0103e50a7756a5
Dmitry <hldimon@mail.ru>
2020-04-13 12:41:33 +0300

    Adding ExecuteCommands constructors.

--- SmartHomeApi.Core.Interfaces/ExecuteCommandResults/ExecuteCommandResultFileContent.cs
+++ SmartHomeApi.Core.Interfaces/ExecuteCommandResults/ExecuteCommandResultFileContent.cs
@@ -2,6 +2,16 @@
 {
     public class ExecuteCommandResultFileContent : ExecuteCommandResultAbstract
     {
+        public ExecuteCommandResultFileContent()
+        {
+        }
+
+        public ExecuteCommandResultFileContent(byte[] content, string contentType)
+        {
+            FileContents = content;
+            ContentType = contentType;
+        }
+
         public byte[] FileContents { get; set; }
         public string ContentType { get; set; }
     }
--- SmartHomeApi.Core.Interfaces/ExecuteCommandResults/ExecuteCommandResultInternalError.cs
+++ SmartHomeApi.Core.Interfaces/ExecuteCommandResults/ExecuteCommandResultInternalError.cs
@@ -4,6 +4,11 @@ namespace SmartHomeApi.Core.Interfaces.ExecuteCommandResults
 {
     public class ExecuteCommandResultInternalError : ExecuteCommandResultAbstract
     {
+        public ExecuteCommandResultInternalError(IList<string> error = null)
+        {
+            Error = error;
+        }
+
         public IList<string> Error { get; set; }
     }
 }
