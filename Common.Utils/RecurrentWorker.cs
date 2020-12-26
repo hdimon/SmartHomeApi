7ab135a13391ae301828dc39a1b2597d05f10769
Dmitry <hldimon@mail.ru>
2020-12-26 18:51:57 +0300

    Implementing recurrent worker.

---
+++ Common.Utils/RecurrentWorker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Common.Utils
+{
+    public class RecurrentWorker
+    {
+        private Task _worker;
+        private readonly CancellationToken _cToken;
+        private readonly Action<Exception> _onException;
+        private readonly Func<CancellationToken, Task> _preAction;
+        private readonly Func<CancellationToken, Task> _action;
+
+        public RecurrentWorker(CancellationToken cToken, Func<CancellationToken, Task> preAction,
+            Func<CancellationToken, Task> action, Action<Exception> onException)
+        {
+            _cToken = cToken;
+            _preAction = preAction;
+            _action = action;
+            _onException = onException;
+        }
+
+        public void Run()
+        {
+            if (_worker == null || _worker.IsCompleted)
+            {
+                _worker = Task.Factory.StartNew(WorkerWrapper).Unwrap().ContinueWith(
+                    t => { _onException?.Invoke(t.Exception); },
+                    TaskContinuationOptions.OnlyOnFaulted);
+            }
+        }
+
+        private async Task WorkerWrapper()
+        {
+            while (!_cToken.IsCancellationRequested)
+            {
+                await _preAction(_cToken);
+
+                try
+                {
+                    await _action(_cToken).ConfigureAwait(false);
+                }
+                catch (Exception e)
+                {
+                    _onException?.Invoke(e);
+                }
+            }
+        }
+    }
+}
