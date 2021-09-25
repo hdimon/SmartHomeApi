1c0d0cbdfbef9a74870910530c2178971e7c07c5
Dmitry <hldimon@mail.ru>
2021-09-25 21:45:53 +0300

    Make order in versions. Preparing to continious integration setup.

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,3 @@
+[assembly: System.Reflection.AssemblyFileVersion("1.3.11.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.3.11")]
+[assembly: System.Reflection.AssemblyVersion("1.3.11.0")]
