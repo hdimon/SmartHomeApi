335543bc2142256b4301429d67f5d11442c32b02
Dmitry <hldimon@mail.ru>
2021-10-25 22:24:06 +0300

    Next version branch.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,3 +1,3 @@
-[assembly: System.Reflection.AssemblyFileVersion("1.3.11.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("1.3.11")]
-[assembly: System.Reflection.AssemblyVersion("1.3.11.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.4.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.4.0")]
+[assembly: System.Reflection.AssemblyVersion("1.4.0.0")]
